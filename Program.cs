25bdde20f009ddd7827a73bd6661fce64d98d980
Ab <meteoritus75@gmail.com>
2024-04-14 23:08:53 +0200

    Challenge tableau

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Un_tableau
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] fruits = { "pommes", "poires", "bananes", "cerises" };
+            string[] legumes = { "carottes", "radis", "courgettes", "poivrons" };
+            string[] fruitsEtLegumes = new string[fruits.Length + legumes.Length]; 
+            Array.Copy(fruits, fruitsEtLegumes, fruits.Length);
+            Array.Copy(legumes,0, fruitsEtLegumes, fruits.Length, legumes.Length);
+
+            foreach (string element in fruitsEtLegumes)
+            {
+                Console.WriteLine(element);
+            }
+
+            
+        }
+    }
+}
